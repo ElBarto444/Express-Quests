a9c44aebbb43745cd69bab187922016999506c4a
S.K.V.666 <melvin.rossignoles@gmail.com>
2023-10-10 11:57:20 +0200

    added POST routes and JSON support

--- userHandlers.js
+++ userHandlers.js
@@ -1,32 +1,52 @@
 const database = require("./database.js");
 
 const getUsers = (req, res) => {
-  database.query("select * from users").then(([users]) => {
-    res.json(users);
-  })
-  .catch((err) => {
-    console.error(err);
-    res.status(500).send("Couldn't retrieve data from DB");
-  });
+  database
+    .query("select * from users")
+    .then(([users]) => {
+      res.json(users);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Couldn't retrieve data from DB");
+    });
 };
 
 const getUserById = (req, res) => {
-    const id = parseInt(req.params.id);
+  const id = parseInt(req.params.id);
 
-    database
-        .query("select * from users where id = ?", [id])
-        .then(([users]) => {
-            users[0] != null
-            ? res.json(users[0])
-            : res.status(404).send("User was not found");
-        })
-        .catch((err) => {
-            console.error(err);
-            res.status(500).send("Couldn't retrieve data from DB")
-        });
+  database
+    .query("select * from users where id = ?", [id])
+    .then(([users]) => {
+      users[0] != null
+        ? res.json(users[0])
+        : res.status(404).send("User was not found");
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Couldn't retrieve data from DB");
+    });
+};
+
+const postUser = (req, res) => {
+  const { firstname, lastname, email, city, language } = req.body;
+
+  database
+    .query(
+      "INSERT INTO users(firstname, lastname, email, city, language) VALUES (?, ?, ?, ?, ?)",
+      [firstname, lastname, email, city, language]
+    )
+    .then(([result]) => {
+      res.location(`/api/users/${result.insertId}`).sendStatus(201);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.status(500).send("Error saving the user");
+    });
 };
 
 module.exports = {
-    getUsers,
-    getUserById,
+  getUsers,
+  getUserById,
+  postUser,
 };
